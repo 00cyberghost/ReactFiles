6deca20d2dc88a3cd564255b3b9a5fa422e2bb2b
ozor clinton <00cyberghost@gmail.com>
2023-12-23 07:13:41 +0100

    sample creation of custom hooks and how to fetch data with it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Cat } from './components/Cat';
+import { QueryClient,QueryClientProvider } from 'react-query';
 
 function App() {
+
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnMount: false
+      }
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QueryClientProvider client={client}>
+        <Cat />
+      </QueryClientProvider>
     </div>
   );
 }
